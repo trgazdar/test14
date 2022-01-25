b268fe8d839bc4d0d05510deb79301eff2e809d5
Aldahir Rojas <aldahir.rojas.m@uni.pe>
2022-01-25 00:34:11 -0500

    Creacion de modelo

---
+++ tabla_control/__manifest__.py
@@ -0,0 +1,9 @@
+{
+    "name":"Tabla de control",
+    "description": "Modulo de control para los clientes",
+    "author":"Aldahir Rojas",
+    "version":"1.0",
+    "depends":["base"],
+    "data":["views/views.xml"]
+
+}
