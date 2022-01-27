28c3eec78026f32589bf6c2c210b70e18c6f1a3c
Aldahir Rojas <aldahir.rojas.m@uni.pe>
2022-01-26 19:03:12 -0500

    Agregar mas cosas

--- tabla_control/models/models.py
+++ tabla_control/models/models.py
@@ -5,22 +5,22 @@ class ControlGuias(models.Model):
     _name="control_guias"
     _description="Control de guias"
 
-    fecha_envio=fields.Datetime("Fecha de envio")
-    fecha_entrega=fields.Datetime("Fecha de entrega")
-    cliente=fields.Char("Cliente")
+    fecha_envio=fields.Date("Fecha de envio")
+    fecha_entrega=fields.Date("Fecha de entrega")
+    cliente_id=fields.Many2one("res.partner","Cliente")
     destino=fields.Char("Destino")
     marca=fields.Char("Marca")
     modelo=fields.Char("Modelo")
     color=fields.Char("Color")
-    destinatario=fields.Char("Destinatario")
+    destinatario_id=fields.Many2one("res.partner","Destinatario")
     cod_ciguena=fields.Char("Cod.Ciguena")
     remitente=fields.Char("G.Rem.Remitente")
-    num_chasis=fields.Integer("Numero de chasis")
+    num_chasis=fields.Char("Numero de chasis")
     pikango=fields.Char("G.R.T Pikango")
     servintes=fields.Char("G.R.T Servintes")
-    cliente_final=fields.Char("Cliente Final")
+    cliente_final_id=fields.Many2one("res.partner","Cliente Final")
     valor=fields.Float("Valor")
-    estado=fields.Selection(selection=[("pagado","Pagado"),("no pagado","No Pagado")],string="Estado de Pago",
+    estado=fields.Selection(selection=[("pagado","Pagado"),("no_pagado","No Pagado")],string="Estado de Pago",
     default="pagado",required=True)
 
 
