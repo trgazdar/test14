90436341e516f2b96355507d4cb4e2fa1063f8e0
Ailton Salguero <ailton.salg@gmail.com>
2022-01-29 21:39:56 -0500

    [FIX] telecr

--- dv_l10n_pe_sunat_ple_13/__manifest__.py
+++ dv_l10n_pe_sunat_ple_13/__manifest__.py
@@ -26,10 +26,10 @@
 	
 	'depends': [
      	'base',
-		'dv_l10n_pe_sunat_ple_base',
+		'dv_l10n_pe_sunat_ple',
 	],
 	'data': [
-     	'security/ple_security.xml',
+     	'security/ple_report_security.xml',
 		'security/ir.model.access.csv',
 		'views/ple_report_views.xml',
 	],
