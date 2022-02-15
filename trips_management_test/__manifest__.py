7cb5f41effd1d3b896fcb50c06da53c4a3f23586
Ailton Salguero <ailton.salg@gmail.com>
2022-02-15 12:48:34 -0500

    [IMP] refactor

--- trips_management_test/__manifest__.py
+++ trips_management_test/__manifest__.py
@@ -24,6 +24,7 @@
     # always loaded
     'data': [
         'views/menus_actions.xml',
+        'views/control_guias_views.xml',
         'views/trip_sourcing.xml',
         'views/trip_section.xml',
         'views/trip_trip.xml',
